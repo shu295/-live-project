99aa3fc868e2ececa154fc42f9939b69681eb4bd
211706129 <60726489+211706129@users.noreply.github.com>
2020-04-08 17:21:29 +0800

    Update live-project.py

--- live-project.py
+++ live-project.py
@@ -1,4 +1,5 @@
 import numpy
+import operator
 def delete(arr,c):
     s=0
     if len(arr)==0 :
@@ -15,23 +16,24 @@ def delete(arr,c):
     
 import time
 import datetime
-s=[(211706127,'ts',datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'2020-04-08 18:11:23'),
-   (211706129,'wcf',datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'2020-04-08 18:11:23'),
-   (211706146,'yl',datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'2020-04-08 18:11:23'),
-   (211706130,'wlp',datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'2020-04-08 18:11:23'),]
-def deng(num,name):  
+def deng(num,name,time1,time2):  
     flag=0
+    t1=time1
     for i in range(len(s)):
-        if s[i][0]==num:#判断列表里面有没有此学号
+        if (s[i][0]==num) & (s[i][3]==''): #判断列表里面有没有此学号且没有写离开时间       
             flag=1#如果有则变为1
     if flag==0:#没有则添加
-        s.append((num,name,datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),))
-deng(211706126,'www')
+        s.append((num,name,t1,time2))
+        return '已登记'
+    else:
+        return '该同学还在学校'
+
 def display(s):
     #排序函数
     sort1(s)
     for i in range(len(s)):
         print(s[i])
+
         
 
 s =input()
@@ -49,22 +51,17 @@ def sort1(s):
     result = sorted(s,key=operator.itemgetter(0))
     for i in range(len(s)):
         print(result[i],'\n')
-   
-import numpy
-s =input()
-s =s.split(",")
-def count1(s):
-    count=0
-    for i in range(len(s)):
-        for j in range(4):
-            if s[i][3]==null:
-                count +=1
-    return count
-print("当前离校人数为"，count1(s))
-print("当前在校人数为"，len(s)-count1(s))
 
-def time_filter(s,st,et):  #通过时间段查找
+        
+def time_filter(s,st,et):
     for i in range(len(s)):
         if (s[i][2]>st):
             if (s[i][2]<et):
                 print(s[i])
+
+def research(x):
+    for i in range(0,len(a)):
+        if x==a[i][0]:
+            print("学号:%d, 姓名:%s, 进校时间:%s, 离校时间:%s"%(a[i][0],a[i][1],a[i][2],a[i][3]))
+   
+
