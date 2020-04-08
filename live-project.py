2e3983811e43deb88b3e0819f1419a8871ca0919
shu295 <51659758+shu295@users.noreply.github.com>
2020-04-08 21:25:27 +0800

    Update live-project.py

--- live-project.py
+++ live-project.py
@@ -40,14 +40,21 @@ def deng(num,name,s):
 #添加离校时间        
 def lixiao(arr,c):
     a=[]
-    res_list = [x[0] for x in arr]#获取学号
-    for i in range(0,len(res_list)):#查找学号下标
-        if res_list[i]==c:
+    s=0
+    num=len(arr)
+    print(num)
+    if num==1:
+        num=num+1
+    for i in range(0,num-1):
+        if arr[i][0]==c:
             a=arr[i]
             arr.pop(i)
             b= list(a)#将元祖转换成列表
             b.append(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-            arr.append(b)
+            arr.append(tuple(b))
+            s=1
+    if(s==0):
+        return '没有找到此记录'
     return arr
 
 def display(s):
@@ -55,34 +62,35 @@ def display(s):
     for i in range(len(s)):
         print(s[i])
 #离校人数
-import numpy
 def count1(s):
     count=0
     for i in range(len(s)):
-        if len(s[i][3])==0:
+        if len(s[i])==4:
             count +=1
     return count
-print("当前在校的人数为",len(s)-count1(s))
-print("当前离校的人数为",count1(s))
-
 
 def sort1(s):
     result = sorted(s,key=operator.itemgetter(0))
     for i in range(len(s)):
         print(result[i],'\n')
 
-        
+#按照登记时间查询        
 def time_filter(s,st,et):
     for i in range(len(s)):
-        if (s[i][2]>st):  #开始时间
-            if (s[i][2]<et):  #末尾时间
+        if (s[i][2]>=st):  #开始时间
+            if (s[i][2]<=et):  #末尾时间
                 print(s[i])
 
-def research(x):
+#按照学号查询
+def research(a,x):
+    flag=0
     for i in range(0,len(a)):
-        if x==a[i][0]:
-            print("学号:%d, 姓名:%s, 进校时间:%s, 离校时间:%s"%(a[i][0],a[i][1],a[i][2],a[i][3]))
-        flag=1
+        if a[i][0]==x:
+            if(len(a[i])==3):
+                 print("学号:%s, 姓名:%s, 进校时间:%s"%(a[i][0],a[i][1],a[i][2]))
+            else:
+                print("学号:%s, 姓名:%s, 进校时间:%s, 离校时间:%s"%(a[i][0],a[i][1],a[i][2],a[i][3]))
+            flag=1
     if flag==0:
         print("未查询到相关信息")
                 
